e733dd028366a88279f2ce561830b23e9cb3a291
Ulyana Yunusova <85567113+Ulyanaaa@users.noreply.github.com>
2023-04-22 19:18:35 +0300

    Add files via upload

---
+++ Lab4(dinamic)/vvod_and_vivod.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+int **vvod(int m, int n)
+{
+	int** mas;
+	mas = new int* [m];
+	for (int i = 0; i < m; i++)
+	{
+		mas[i] = new int[n];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < m; k++)
+		{
+			cin >> *(*(mas + i) + k);
+		}
+	}
+	return mas;
+}
+
+
+void vivod_res(int m, int n, int **res_mas)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			cout << *(*(res_mas +i)+k) << " ";
+		}
+		cout << endl;
+	}
+}
